b246782fa2da6bb6f8ba4168b0522c5e3ffe9771
Twinkal Parmar <twinkal@promactinfo.com>
2018-06-18 18:05:59 +0530

    added

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("how many hours of sleep did you get last night??");
+            int hours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+
+            if (hours > 8)
+            {
+                Console.WriteLine("well rested...");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep..");
+            }
+
+
+        }
+    }
+}
